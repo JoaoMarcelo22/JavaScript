8a8c7c1e56dc72a8efe24d04416c67c98829cf31
João Marcelo <joao_marcelo.22@hotmail.com>
2021-08-12 11:37:14 -0300

    Update CaixaMensagem.js

--- CaixaMensagem.js
+++ CaixaMensagem.js
@@ -1,1 +1,5 @@
-alert("Seja bem vindo ao site");
+alert("Seja bem vindo ao site"); //comando alert//
+let nome = prompt("Qual o seu nome?"); //comando prompt//
+document.write(nome+",seja bem vindo ao site!");
+let opcao = confirm("Deseja encerrar?"); //comando confirm//
+document.write("Sua escolha foi= "+ opcao);
