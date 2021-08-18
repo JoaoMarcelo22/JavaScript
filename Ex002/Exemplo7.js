80815cddb121bf14e9b1ced836aa34dc596be1e9
João Marcelo <joao_marcelo.22@hotmail.com>
2021-08-18 10:11:53 -0300

    incremento  e decremento

---
+++ Ex002/Exemplo7.js
@@ -0,0 +1,25 @@
+let contador = 0;
+contador += 1;//contem o valor 1//
+contador += 1;//contem o valor 2//
+contador += 1;//contem o valor 3//
+contador += 1;//contem o valor 4//
+
+let contador = 0;
+contador++; // contem o valor 1//
+contador++; // contem o valor 2//
+contador++; // contem o valor 3//
+contador++; // contem o valor 4//
+//--Incremento--//
+
+let tentativas =3;
+tentativas--; // passa a valer 2//
+tentativas--; // passa a valer 1/
+tentativas--; // passa a valer 0//
+//-- Decremento--//
+
+let contador = 10;
+console.log(contador++);// exibe o valor 10 no console//
+//pós-cremento//
+console.log(++contador);// exibe o valor 12 no console//
+//pré-cremento//
+
