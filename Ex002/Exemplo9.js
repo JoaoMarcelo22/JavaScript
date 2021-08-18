b2141f7fb4a6595c563908f77b722068f4291da9
João Marcelo <joao_marcelo.22@hotmail.com>
2021-08-18 10:52:37 -0300

    Create Exemplo9.js

---
+++ Ex002/Exemplo9.js
@@ -0,0 +1,77 @@
+//-- Iguaal--//
+//operação 1//
+console.log(10==10);//TRUE//
+// tem o mesmo valor e são do mesmo tipo//
+
+//operação 2//
+console.log(15=='15');//TRUE//
+// tem o mesmo valor e são de tipos diferentes//
+
+
+
+//-- Estritamente igual--//
+//operação 1//
+console.log(30===30);//TRUE//
+//tem o mesmo valor e são de tipo iguais//
+
+//operação 2//
+console.log(100==="100")//FALSE//
+//tem o mesmo valor e são de tipos diferentes//
+
+
+
+//--Diferente--//
+let a=5;
+let b='5';
+let c=7;
+
+//comparação 1//
+console.log(a!=5);//FALSE//
+//são do mesmo valor e do mesmo tipo//
+
+//comparação 2//
+console.log(a!=b);//FALSE//
+//sao do mesmo valor e de tipos diferente//
+
+//comparação 3//
+console.log(a!=c);//TRUE//
+//sao de valores e tipos diferente//
+
+
+
+//-- Estritamente Diferente--//
+let a=5;
+let b='5';
+let c=7;
+
+//comparação 1//
+console.log(a!=5);//FALSE//
+//são do mesmo valor e do mesmo tipo//
+
+//comparação 2//
+console.log(a!=b);//TRUE//
+//sao do mesmo valor e de tipos diferente//
+
+//comparação 3//
+console.log(a!=c);//TRUE//
+//sao de valores e tipos diferente//
+
+//-- Maior que--//
+console.log(6>7) //FALSE//
+
+console.log(10>5) //TRUE//
+
+//-- Maior que ou igual--//
+console.log(5>=5) //TRUE//
+
+console.log(10>5) //TRUE//
+
+//-- Menor que--//
+console.log(8<15) //TRUE//
+
+console.log(5<3) //FALSE//
+
+//-- Menor que ou igual--//
+console.log(15<=15) //TRUE//
+
+console.log(1<=3) //TRUE//
