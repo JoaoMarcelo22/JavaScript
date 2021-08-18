6d7e7a7281eb713f3cbdc262fac3234f147435ee
João Marcelo <joao_marcelo.22@hotmail.com>
2021-08-18 01:17:15 -0300

    variaveis
    const or let

---
+++ Ex002/Exemplo4.js
@@ -0,0 +1,6 @@
+let nome="maria";
+nome="jose";
+// a variavel let é maleavel podendo possuir novas formas e navegando entre todos os tipos de dados//
+
+const nome1="joao";
+// a variavel let, como o proprio nome ja diz, ela é constante, ou seja, não pode ser modificada//
