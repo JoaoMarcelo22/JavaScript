bfbc32679d88416cafbbd946ff810869c19dd699
João Marcelo <joao_marcelo.22@hotmail.com>
2021-08-18 01:22:23 -0300

    Update Exemplo1.js

--- Ex002/Exemplo1.js
+++ Ex002/Exemplo1.js
@@ -4,3 +4,4 @@ console.log(typeof(idade));
 idade="vinte e cinco";
 console.log(typeof(idade));
 // mesma variavel com tipo de dados diferentes, number e string//
+//typeof  é usado para exiber o tipo de dado string,number// 
