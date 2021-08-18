1b5077035197c76ae85e2ecb786dbc6ebded1271
João Marcelo <joao_marcelo.22@hotmail.com>
2021-08-18 10:21:01 -0300

    Operadores de comparação

---
+++ Ex002/Exemplo8.js
@@ -0,0 +1,24 @@
+"6"==6
+  6==6
+//o simbulo de igual é representado por ==//
+6===6
+//o simbulo de estritamente igual ou identico ===//
+
+6!=5
+6!="5"
+// o simbulo de não igual e diferente é representado por !=//
+
+6!==5
+//o simbulo de estritamente não igual é representado por  !==//
+
+6>5
+// simbulo de maior que//
+
+6>=5
+// simbulo de maior ou igual//
+
+5<6
+// simbulo de menor//
+
+5<=6
+// simbulo de menor ou igual//
