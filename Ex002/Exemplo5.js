d3015b7b86d046ae9551af99d869d6fad5ad6362
João Marcelo <joao_marcelo.22@hotmail.com>
2021-08-18 01:28:23 -0300

    prencipais atribuições

---
+++ Ex002/Exemplo5.js
@@ -0,0 +1,8 @@
+let x=2;
+// atribuição de um numero inteiro//
+ let float=2.5;
+ // atribuição de um numero real, ou com ponto flutuante//
+ let string="Uma string aqui";
+ // atribuição a uma string//
+ let bollean = false;
+ // atribuição de um valor bolleano//
