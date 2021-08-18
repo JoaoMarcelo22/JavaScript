59abbbad6fab48e3ede7f0814746b05298809d5a
João Marcelo <joao_marcelo.22@hotmail.com>
2021-08-18 01:42:58 -0300

    operadores

---
+++ Ex002/Exemplo6.js
@@ -0,0 +1,42 @@
+let x =5;
+let y =2;
+
+x = x + y;
+// operação 1//
+
+x += y;
+//operação 2//
+//-- Adição--//
+
+
+
+let x=5;
+let y=2;
+
+x = x - y;
+//operação 1//
+
+x -=y;
+//operação 2//
+//--Subtração--//
+
+
+let x=5;
+let y=5;
+
+x = x * y;
+//operação 1//
+
+x *= y;
+//operação2//
+//--Multiplicação--//
+
+let x=10;
+let y=5;
+
+x = x / y;
+//operação 1//
+
+x /= y;
+//operação 2//
+//--Divisão--//
