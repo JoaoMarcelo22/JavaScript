d3901e7b09c8bc2f39cc2aba5e781ba5ecb10b12
João Marcelo <joao_marcelo.22@hotmail.com>
2021-08-20 01:17:50 -0300

    AND. OR e NOT

--- Ex002/Exemplo10.js
+++ Ex002/Exemplo10.js
@@ -1,3 +1,25 @@
 1 && 2; //simbulo "AND"//
 1 || 2; //simbulo "OR"//
-1 ! 2; //simbulo de "NOT"//
+ //.. ! ..simbulo de "NOT"//
+
+ let escolha = 23; //escolha do usuario//
+ console.log(escolha >= 10 && escolha <=20); // exibe False no console//
+ escolha = 17;// outra escolha
+ console.log(escolha >= 10 && escolha <=20); //exibe True no console//
+
+ //--------------------AND------------------------------------------//
+
+ let escolha = 23; // escolha do usuario//
+ console.log(escolha < 10 || escolha >20); // exibe True no console//
+ escolha = 17; // outra escolha//
+ console.log(escolha < 10 || escolha >20); // exibe False no console//
+ 
+ //--------------------OR------------------------------------------//
+
+ let usuarioLogado = false;
+ console.log(!usuarioLogado); // exibe True no console//
+
+ let escolha = 5;
+ console.log(!(escolha < 10)); // exibe False no console//
+
+ //--------------------NOT------------------------------------------//
