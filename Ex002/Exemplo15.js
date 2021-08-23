f21813bd15991423077a4d121514a4dd145cfef7
João Marcelo <joao_marcelo.22@hotmail.com>
2021-08-23 19:01:51 -0300

    Dom

--- Ex002/Exemplo15.js
+++ Ex002/Exemplo15.js
@@ -1,1 +1,25 @@
 //---------------------------DOM-------------//
+
+//-------------------------OB-WINDOW---------//
+console.log(window.innerWidth); // Mostra o tamanho atual da tela//
+console.log(screen.width); // mostra a resolução do monitor//
+
+
+window.scroll(0,window.innerWidth);
+//-----------------------OB-DOCUMENT---------//
+
+
+//------------------------OB-ELEMENT---------//
+document.onscroll = function(){
+    console.log('rolando');
+}
+
+
+//Eventos//
+onclick("quando clicado")
+onload("quando carregado")
+onresize("quando redimencionado")
+onchange("quando um elemento HTML for alterado")
+onmouseover("quando passar o mouser 'por cima'do elemento")
+onkeydown("quando o usuario pressionar alguma tecla do teclado")
+onmouseout("quando o usuario movimentar o mouse 'para fora' do elemento")
