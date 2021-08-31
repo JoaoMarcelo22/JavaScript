7f8b7ce204a796fd9fc9cb2934522a4747868849
João Marcelo <joao_marcelo.22@hotmail.com>
2021-08-31 11:39:08 -0300

    Update Exemplo19.js

--- Ex002/Exemplo19.js
+++ Ex002/Exemplo19.js
@@ -8,3 +8,14 @@ if(idade<16){
 } else{
     console.log('Voto Obrigatório')
 }
+//------------------------exemplo 2-------------------------------//
+let agora = new Date()
+let hora = agora.getHours()
+console.log(`Agora são exatamente ${hora} horas.`)
+if (hora<12){
+    console.log('Bom dia')
+} else if(hora <=18){
+    console.log('Boa tarde')
+} else{
+    console.log('Boa noite')
+}
