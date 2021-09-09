721e9e2ba6445cea5243d073ad120b721587128c
João Marcelo <joao_marcelo.22@hotmail.com>
2021-09-09 11:00:48 -0300

    imc + if else

---
+++ Ex003-visualg-JS/Ex007.js
@@ -0,0 +1,9 @@
+let Massa = 60
+let Altura = 1.60
+let imc = Massa/(Altura^2)
+if( imc >= 18.5 & imc <25){
+    console.log("Parabens! você esta no seu peso ideal")
+}
+else{
+    console.log("Você não esta na faixa de peso ideal")
+}
