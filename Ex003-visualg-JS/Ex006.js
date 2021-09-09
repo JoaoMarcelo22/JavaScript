33257be0915b825daa4672141990738db5a85b7c
João Marcelo <joao_marcelo.22@hotmail.com>
2021-09-09 10:54:20 -0300

    idade + If e Else

--- Ex003-visualg-JS/Ex006.js
+++ Ex003-visualg-JS/Ex006.js
@@ -1,1 +1,10 @@
-let 
+let AnoAtual = 2021 
+let AnoNasc = 1998
+let idade = AnoAtual - AnoNasc
+console.log("Em " + AnoAtual , " Você tera " +idade ," Anos")
+if( idade>= 21){
+    console.log( "e já tera a maioridade.")
+}
+else{
+    console.log( "vc ainda será menor de idade.")
+}
