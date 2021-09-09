16945d51ca86452357d6b7de350dccbd297bdc7b
João Marcelo <joao_marcelo.22@hotmail.com>
2021-09-09 12:06:44 -0300

    switch

---
+++ Ex003-visualg-JS/Ex011.js
@@ -0,0 +1,11 @@
+let salario =1500
+let nome = "joao"
+let depedente = 2
+let Nsal=0
+switch (depedente){
+    case 1
+       Nsal = salario + (salario*5/100)
+        console.log("novo salario de " + nome ," Será de"+ Nsal)
+    break
+
+}
